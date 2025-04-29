dc7c875a7e34399ec2755da14d8b32927021dacb
Reuven Lerner <reuven@lerner.co.il>
2025-04-29 21:22:03 +0300

    gitautosync update 68

--- mymod.py
+++ mymod.py
@@ -1,4 +1,5 @@
-print(f'Hello from {__name__}!')  # __name__ is a variable, same as mymod.__name__ outside 
+if __name__ == '__main__':
+    print(f'Hello from {__name__}!')  # only print when running the module; don't print when we import it
 
 x = 100
 
@@ -7,4 +8,6 @@ y = [10, 20, 30]
 def hello(name):
     return f'Hello, {name} from mymod!'
 
-print(f'Goodbye from {__name__}!')
+if __name__ == '__main__':
+    print(f'Goodbye from {__name__}!')   # only print when running the module as a program; don't when we import it
+    
